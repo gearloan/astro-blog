c71a3745348ff1c6cae115f6d792c50df82809e0
gearloan <johnsfuller@gmail.com>
2025-07-15 17:18:01 -0400

    added wp-graphql queries

--- src/lib/fetchPosts.js
+++ src/lib/fetchPosts.js
@@ -1,16 +1,35 @@
-// src/lib/fetchPosts.js
 export async function fetchPosts() {
-  const res = await fetch('http://167.99.153.17/wp-json/wp/v2/posts?_embed');
-  if (!res.ok) throw new Error('Failed to fetch posts');
-  const posts = await res.json();
-
-  return posts.map((post) => {
-    const featuredImage =
-      post._embedded?.['wp:featuredmedia']?.[0]?.media_details?.sizes?.thumbnail?.source_url ?? null
-
-    return {
-      ...post,
-      featured_image: featuredImage,
-    };
+  const res = await fetch('http://your-wp-site.com/graphql', {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify({
+      query: `
+        query {
+          posts(first: 10) {
+            nodes {
+              id
+              slug
+              title
+              excerpt
+              date
+              featuredImage {
+                node {
+                  sourceUrl
+                  mediaDetails {
+                    sizes {
+                      name
+                      sourceUrl
+                    }
+                  }
+                }
+              }
+            }
+          }
+        }
+      `
+    })
   });
+
+  const json = await res.json();
+  return json.data.posts.nodes;
 }
