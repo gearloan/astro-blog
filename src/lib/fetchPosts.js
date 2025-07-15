6cf561c27a0e47e1f21e7255762d5b7e6a79ccea
gearloan <johnsfuller@gmail.com>
2025-07-15 16:28:43 -0400

    added images

--- src/lib/fetchPosts.js
+++ src/lib/fetchPosts.js
@@ -1,9 +1,16 @@
 // src/lib/fetchPosts.js
 export async function fetchPosts() {
-  const res = await fetch("http://167.99.153.17/wp-json/wp/v2/posts/");
-  if (!res.ok) {
-    throw new Error(`Failed to fetch posts: ${res.status}`);
-  }
-  return await res.json();
-}
+  const res = await fetch('http://167.99.153.17/wp-json/wp/v2/posts?_embed');
+  if (!res.ok) throw new Error('Failed to fetch posts');
+  const posts = await res.json();
 
+  return posts.map((post) => {
+    const featuredImage =
+      post._embedded?.['wp:featuredmedia']?.[0]?.media_details?.sizes?.thumbnail?.source_url ?? null
+
+    return {
+      ...post,
+      featured_image: featuredImage,
+    };
+  });
+}
