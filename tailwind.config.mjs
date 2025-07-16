dad9f07c1244cd400e6936b10c3592c4a10ca122
gearloan <johnsfuller@gmail.com>
2025-07-15 21:16:47 -0400

    configured tailwind

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,10 +1,15 @@
-import type { Config } from 'tailwindcss'
-
+/** @type {import('tailwindcss').Config} */
 export default {
-  content: [],
+  content: ['./src/**/*.{astro,html,js,jsx,ts,tsx,md,mdx}'],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        // your font config
+      },
+      colors: {
+        'aopa-dkblue': '#002a3a',
+      },
+    },
   },
   plugins: [],
-} satisfies Config
-
+};
